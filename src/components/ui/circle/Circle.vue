a3d7a01a2e2bef19cb7fc8c85f6e2cc28b2cf44e
kjmj <kmorton15@gmail.com>
2024-03-24 17:20:06 -0600

    create Circle.vue

---
+++ src/components/ui/circle/Circle.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  isActive: boolean;
+}>();
+</script>
+
+<template>
+  <span class="circle" :class="isActive ? 'active' : ''">
+    <slot />
+  </span>
+</template>
+
+<style scoped>
+.circle {
+  display: grid;
+  place-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  border: 1px solid;
+  color: theme("colors.primary.foreground");
+  transition: all 0.25s ease-in-out;
+}
+
+.active {
+  color: theme("colors.primary.DEFAULT");
+  border: 1px solid theme("colors.primary.foreground");
+  background-color: theme("colors.primary.foreground");
+}
+</style>
