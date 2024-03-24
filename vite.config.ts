19d71ff2fa75844fffb017071ed0e2440b7d7971
kjmj <kmorton15@gmail.com>
2024-03-23 18:12:15 -0600

    complete installation and configuration for tailwind and shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue(), eslint()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
