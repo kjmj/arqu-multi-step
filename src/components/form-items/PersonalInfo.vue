9d793a5fd817e6200785010fd16266ca96826b83
kjmj <kmorton15@gmail.com>
2024-03-25 14:19:55 -0600

    show an input on step 1

--- src/components/form-items/PersonalInfo.vue
+++ src/components/form-items/PersonalInfo.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import FormContentContainer from "@/components/ui/form-content-container/FormContentContainer.vue";
+import LabeledInput from "@/components/ui/labeled-input/LabeledInput.vue";
 </script>
 
 <template>
   <FormContentContainer title="Step 1 title" subtitle="Step 1 subtitle">
-    <div>step 1 content</div>
+    <LabeledInput
+      id="name-input"
+      label="Name"
+      placeholder="Jane Doe"
+    ></LabeledInput>
   </FormContentContainer>
 </template>
 
