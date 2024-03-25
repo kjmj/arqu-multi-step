3967b5aead355fb78e0e6d4a32f9200fd8e4d1bc
kjmj <kmorton15@gmail.com>
2024-03-25 14:54:43 -0600

    create a basic PlanCard.vue

---
+++ src/components/ui/plan-card/PlanCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  name: string;
+  price: number;
+  isMonthly: boolean;
+}>();
+
+const priceText = computed(() => {
+  return props.isMonthly ? `${props.price}/month` : `${props.price}/year`;
+});
+</script>
+
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div>{{ priceText }}</div>
+  </div>
+</template>
+
+<style scoped></style>
