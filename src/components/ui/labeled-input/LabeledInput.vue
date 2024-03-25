c60d5f2f02adc4172926250465b3fa83f5dd12b7
kjmj <kmorton15@gmail.com>
2024-03-24 19:53:05 -0600

    create LabeledInput.vue

---
+++ src/components/ui/labeled-input/LabeledInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Label from "@/components/ui/label/Label.vue";
+import Input from "@/components/ui/input/Input.vue";
+
+defineProps<{
+  id: string;
+  label: string;
+  placeholder: string;
+}>();
+</script>
+
+<template>
+  <div class="grid w-full max-w-sm items-center gap-1.5">
+    <Label :for="id">{{ label }}</Label>
+    <Input :id="id" :placeholder="placeholder" v-bind="$attrs" />
+  </div>
+</template>
+
+<style scoped></style>
