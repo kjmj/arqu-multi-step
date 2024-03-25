f838d446c9602c9a9329b37c82f7bfdc1d6dc969
kjmj <kmorton15@gmail.com>
2024-03-25 14:39:34 -0600

    make the summary a bit easier to read

--- src/components/form-items/Summary.vue
+++ src/components/form-items/Summary.vue
@@ -5,8 +5,9 @@ import CheckoutStore from "@/store/CheckoutStore";
 
 <template>
   <FormContentContainer title="Step 4 title" subtitle="Step 4 subtitle">
-    <div>step 4 content</div>
-    <div>{{ CheckoutStore.state }}</div>
+    <div>Your name: {{ CheckoutStore.state.name }}</div>
+    <div>Plan: {{ CheckoutStore.state.plan.name }}</div>
+    <div>Add Ons: {{ CheckoutStore.state.addons.map((a) => a.name) }}</div>
   </FormContentContainer>
 </template>
 
