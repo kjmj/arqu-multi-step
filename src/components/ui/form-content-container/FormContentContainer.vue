f9b48ed9a2512f4e15fd57551d56f57f08ba8aba
kjmj <kmorton15@gmail.com>
2024-03-24 19:04:32 -0600

    create FormContentContainer.vue which will standardize each page of the form

---
+++ src/components/ui/form-content-container/FormContentContainer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  subtitle: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ subtitle }}</p>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
