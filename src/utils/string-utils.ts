dd383b954982bd230b0d055c1e525e701dd07a13
kjmj <kmorton15@gmail.com>
2024-03-26 10:59:51 -0600

    create to0BasedIdx function

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -2,6 +2,10 @@ export function to1BasedIdx(idx: number): number {
   return idx + 1;
 }
 
+export function to0BasedIdx(idx: number): number {
+  return idx - 1;
+}
+
 export function priceText(isYearly: boolean, price: number): string {
   return isYearly ? `$${price}/year` : `$${price}/month`;
 }
