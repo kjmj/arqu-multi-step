fd744349741efdcb937b6dcbf0bd6157e4c83143
kjmj <kmorton15@gmail.com>
2024-03-25 21:55:19 -0600

    create a basic submit page

--- src/components/form-items/Submit.vue
+++ src/components/form-items/Submit.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
 
 <template>
-  <div>submit</div>
+  <div class="flex flex-col items-center text-center">
+    <Icon
+      icon="radix-icons:check-circled"
+      class="h-[4.5rem] w-[4.5rem] mb-4"
+      color="#15c260"
+    />
+    <h1 class="text-xl font-bold mb-2">Thank you!</h1>
+    <p>Thanks for your purchase. We hope you love our service!</p>
+  </div>
 </template>
 
 <style scoped></style>
