26261fbe34e244114c02e9bfb462daa26a50dc64
kjmj <kmorton15@gmail.com>
2024-03-25 21:06:09 -0600

    add checkbox to AddOnCard.vue

--- src/components/ui/add-on-card/AddOnCard.vue
+++ src/components/ui/add-on-card/AddOnCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { AddOn } from "@/types/AddOn";
 import { priceText } from "@/utils/string-utils";
+import Checkbox from "@/components/ui/checkbox/Checkbox.vue";
 const props = defineProps<{
   addon: AddOn;
   price: number;
@@ -14,9 +15,10 @@ const props = defineProps<{
     class="rounded-md add-on-card text-left p-3 cursor-pointer"
     :class="props.isSelected ? 'active' : ''"
   >
-    <div class="flex">
+    <div class="flex items-center">
+      <Checkbox :checked="props.isSelected" class="mr-4" />
       <div class="grow">
-        <div class="text-lg font-bold">{{ props.addon.name }}</div>
+        <div class="text-md font-bold">{{ props.addon.name }}</div>
         <div>{{ props.addon.description }}</div>
       </div>
       <div>{{ priceText(isYearly, price) }}</div>
