6640a38acfc9b2fe88b6be98b496c9d028c9ddc4
kjmj <kmorton15@gmail.com>
2024-03-26 13:16:56 -0600

    make input border/ring red if there is an error

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -2,6 +2,7 @@
 import type { HTMLAttributes } from "vue";
 import { useVModel } from "@vueuse/core";
 import { cn } from "@/lib/utils";
+import { useFormField } from "@/components/ui/form/useFormField";
 
 const props = defineProps<{
   defaultValue?: string | number;
@@ -18,6 +19,8 @@ const modelValue = useVModel(props, "modelValue", emits, {
   passive: true,
   defaultValue: props.defaultValue,
 });
+
+const { error, formItemId } = useFormField();
 </script>
 
 <template>
@@ -26,8 +29,10 @@ const modelValue = useVModel(props, "modelValue", emits, {
     :class="
       cn(
         'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
+        error && 'border border-destructive focus-visible:ring-destructive',
         props.class,
       )
     "
+    :for="formItemId"
   />
 </template>
