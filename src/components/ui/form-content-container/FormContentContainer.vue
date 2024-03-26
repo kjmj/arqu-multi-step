9d2ba6651eb4a9bc5a979d5c00ba746abaddd41c
kjmj <kmorton15@gmail.com>
2024-03-25 18:30:27 -0600

    adjust padding around form content

--- src/components/ui/form-content-container/FormContentContainer.vue
+++ src/components/ui/form-content-container/FormContentContainer.vue
@@ -9,7 +9,7 @@ defineProps<{
   <div>
     <h1 class="text-3xl text-primary font-bold">{{ title }}</h1>
     <p class="text-light_gray mb-8">{{ subtitle }}</p>
-    <slot />
+    <div class="p-1"><slot /></div>
   </div>
 </template>
 
