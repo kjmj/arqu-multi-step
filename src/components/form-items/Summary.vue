d548f1d1b937cb5057a53113f08cc17069004d68
kjmj <kmorton15@gmail.com>
2024-03-25 21:46:43 -0600

    create a basic summary page

--- src/components/form-items/Summary.vue
+++ src/components/form-items/Summary.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import FormContentContainer from "@/components/ui/form-content-container/FormContentContainer.vue";
-import CheckoutStore from "@/store/CheckoutStore";
+import { priceText } from "@/utils/string-utils";
+import checkoutStore from "@/store/CheckoutStore";
 </script>
 
 <template>
@@ -8,12 +9,51 @@ import CheckoutStore from "@/store/CheckoutStore";
     title="Finishing up"
     subtitle="Please make sure everything looks good before continuing"
   >
-    <div>Your name: {{ CheckoutStore.state.name }}</div>
-    <div>
-      Plan: {{ CheckoutStore.state.plan.name }}, yearly?
-      {{ CheckoutStore.state.isBilledYearly }}
+    <div class="bg-primary-light rounded-md">
+      <div>{{ checkoutStore.state.name }}</div>
+      <div>-----</div>
+      <div class="flex justify-between">
+        <div>{{ checkoutStore.state.plan.name }}</div>
+        <div>
+          {{
+            priceText(
+              checkoutStore.state.isBilledYearly,
+              checkoutStore.methods.planPrice(),
+            )
+          }}
+        </div>
+      </div>
+      <div>-----</div>
+      <div v-if="checkoutStore.state.addons.length > 0">
+        <div
+          v-for="addon in checkoutStore.state.addons"
+          :key="addon"
+          class="flex justify-between"
+        >
+          <div>{{ addon.name }}</div>
+          <div>
+            {{
+              priceText(
+                checkoutStore.state.isBilledYearly,
+                checkoutStore.methods.addOnPrice(addon),
+              )
+            }}
+          </div>
+        </div>
+      </div>
     </div>
-    <div>Add Ons: {{ CheckoutStore.state.addons.map((a) => a.name) }}</div>
+    <div class="flex justify-between">
+      <div>Total</div>
+
+      <div>
+        {{
+          priceText(
+            checkoutStore.state.isBilledYearly,
+            checkoutStore.methods.totalPriceSelected(),
+          )
+        }}
+      </div>
+    </div>
   </FormContentContainer>
 </template>
 
